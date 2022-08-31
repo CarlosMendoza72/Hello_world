40e0958f9fe5789ea730e5015478a15fd5168075
CarlosMendoza72 <106283922+CarlosMendoza72@users.noreply.github.com>
2022-08-30 21:33:33 -0500

    Add files via upload

---
+++ voy_a_adivinar_tu_número.py
@@ -0,0 +1,31 @@
+# 1. Piensa un número, voy a adivinarlo 
+miNumeroEs = input("Ingresa un número: ")
+miNumeroEs = int (miNumeroEs)
+
+# 2. Multiplícalo por 5 
+miNumeroEs *= 5
+print("El resultado de multiplicarlo por 5 es:", miNumeroEs)
+
+# 3. A lo que quedó, súmale 12 
+miNumeroEs += 12
+print("El resultado de sumarle 12 es:", miNumeroEs)
+
+# 4. Lo que quedó multiplícalo por 10
+miNumeroEs *= 10
+print("El resultado de multiplicarlo por 10 es:", miNumeroEs)
+
+# 5. A lo que quedó súmale 5 
+miNumeroEs += 5
+print("El resultado de sumarle 5 es:", miNumeroEs)
+
+# 6. Lo que quedó multiplícalo por 2
+miNumeroEs *= 2
+print("El resultado de multiplicarlo por 2 es:", miNumeroEs)
+
+# 7. restarle 250 
+miNumeroEs -= 250
+print("El resultado de restarle 250 es:", miNumeroEs)
+
+# 8. dividirlo entre 100
+miNumeroEs /= 100
+print("El resultado de dividirlo entre 100 es el número que pensaste:", miNumeroEs)
