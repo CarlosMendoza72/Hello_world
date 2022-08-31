a3ffe36b985ed86590bc6de08c51c485008ae5cb
CarlosMendoza72 <106283922+CarlosMendoza72@users.noreply.github.com>
2022-08-30 21:33:05 -0500

    Add files via upload

---
+++ pensar_en_un_numero.py
@@ -0,0 +1,28 @@
+# 1. Piensa un número
+print("Piense en un número:")
+num = 3
+
+#x = y
+#x = num1
+#y = num
+#El valor de y se mantiene, mientras x se actualiza
+
+
+# 2. Multiplícalo por 3 
+num *= 3
+
+# 3. A lo que quedó súmale 14  
+num += 14
+
+# 4. Al resultado súmale el número que pensaste 
+num += num
+
+# 5. A lo que quedó réstale 2 
+num -= 2
+
+# 6. El resultado divídelo entre 4 
+num /= 4
+
+# 7. A lo que quedó réstale 3
+num -= 3
+print("Su número es:", num)
